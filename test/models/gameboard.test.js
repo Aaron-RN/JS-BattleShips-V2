72780b8a6473b0de2a4e1663adc55c36b35674ab
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-02-13 16:53:48 -0500

    Update gameboard.test.js

--- test/models/gameboard.test.js
+++ test/models/gameboard.test.js
@@ -1,3 +1,13 @@
-test('it shuts the fuck up about empty files', () => {
-  expect(1).toBe(1);
+import Ship from '../../src/models/ship';
+import GameBoard from '../../src/models/gameboard';
+
+const size = 10;
+const gameBoard = new GameBoard(size);
+//placeShip(ship, x, y, direction)
+
+describe('#Gameboard', () => {
+  test('places a ship', () => {
+//    gameBoard.placeShip();
+    expect(gameBoard.getBoard()).toHaveLength(size);
+  });
 });
